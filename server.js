bddccaa4c04d6b7087b4a8526210aa83602017f1
Nick Lewis <nick@o3world.com>
2019-08-26 19:08:17 -0400

    Lets just hope this goes well

---
+++ server.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+var backstop = require('./runner')
+// Create a new instance of express
+const app = express()
+
+// Tell express to use the body-parser middleware and to not parse extended bodies
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// Route that receives a POST request to /sms
+app.get('/', function (req, res) {
+  let testName = req.query.testName;
+  let referenceName = req.query.referenceName;
+  let command = req.query.command;
+  let url = req.query.url;
+  let response = backstop.runner({
+    'url': req.query.url,
+    'command': req.query.command,
+    'scenario': req.query.scenario
+  });
+  res.set('Content-Type', 'text/plain')
+  res.send(`You sent: ${response} to Express`)
+});
+
+// Tell our app to listen on port 3000
+app.listen(4242, function (err) {
+  if (err) {
+    throw err
+  }
+
+  console.log('Server started on port 3000')
+})
